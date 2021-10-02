5a9e114dcc01f2cdf4cc9c7cf237d65da807821b
alprsln <alpars44@hotmail.com>
2021-10-02 09:22:05 +0300

    feat!: add new models

--- Product.Domain/Models/Product.cs
+++ Product.Domain/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,10 @@ namespace ProductNS.Domain.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
-        //TODO: Make the category enum
-        public string Category { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+        [ForeignKey("BrandId")]
+        public virtual Brand Brand { get; set; }
         public DateTime DateOfCreation { get; set; }
         public DateTime DateOfLastEdit { get; set; }
     }
--- Product.Infrastructure/Context/ProductContext.cs
+++ Product.Infrastructure/Context/ProductContext.cs
@@ -17,6 +17,10 @@ namespace ProductNS.Infrastructure.Context
             AddDummyData();
         }
 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         private void AddDummyData()
         {
             //This is a very bad way of filling the db,
@@ -24,6 +28,36 @@ namespace ProductNS.Infrastructure.Context
             //TODO: Refractor this ASAP
             if (Products.Any())
                 return;
+            Categories.AddRange(
+                new Category
+                {
+                    Name = "Furniture",
+                    Description = "Consists of large objects such as tables, chairs, " +
+                    "or beds that are used in a room for sitting or lying on " +
+                    "or for putting things on or in."
+                },
+                new Category
+                {
+                    Name = "Electronics",
+                    Description = "is the technology of using transistors and silicon chips, " +
+                    "especially in devices such as radios, televisions, and computers."
+                }
+            );
+
+            Brands.AddRange(
+                new Brand
+                {
+                    Name = "Ikea",
+                    Description = "IKEA is a global destination store for home furnishing, " +
+                    "appliances, ready-to-assemble furniture, home accessories and kitchen products."
+                },
+                new Brand
+                {
+                    Name = "Big Phones",
+                    Description = " designs, manufactures and markets smartphones, personal computers, " +
+                    "tablets, wearables and accessories, and sells a variety of related services. "
+                }
+            );
 
             DateTime now = DateTime.Now;
             Products.AddRange(
@@ -31,7 +65,8 @@ namespace ProductNS.Infrastructure.Context
                 {
                     Name = "Chair",
                     Description = "A chair",
-                    Category = "Furniture",
+                    Category = Categories.Find(1),
+                    Brand = Brands.Find(1),
                     Price = 50,
                     DateOfCreation = now,
                     DateOfLastEdit = now
@@ -40,7 +75,8 @@ namespace ProductNS.Infrastructure.Context
                 {
                     Name = "Sofa",
                     Description = "A sofa",
-                    Category = "Furniture",
+                    Category = Categories.Find(1),
+                    Brand = Brands.Find(1),
                     Price = 100,
                     DateOfCreation = now,
                     DateOfLastEdit = now
@@ -49,15 +85,14 @@ namespace ProductNS.Infrastructure.Context
                 {
                     Name = "Huge Phone",
                     Description = "The biggest phone yet",
-                    Category = "Electronics",
+                    Category = Categories.Find(2),
+                    Brand = Brands.Find(2),
                     Price = 5000,
                     DateOfCreation = now,
                     DateOfLastEdit = now
                 }
-                );
-            this.SaveChanges();
+            );
+            SaveChanges();
         }
-
-        public DbSet<Product> Products { get; set; }
     }
 }
